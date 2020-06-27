d7042e386bbcba1c3fe3e69ed6c6a28ebd18f747
tmsnvk <contact@tamasnovak.net>
2020-06-27 21:27:28 +0200

    fixes

---
+++ client/build/precache-manifest.fc95e08ea710faa39937574cacc50be6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920f27de6729df2c185b6e07428b4460",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9537b72729431b3a389",
+    "url": "/static/js/2.97b4d89b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.97b4d89b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b070a201e341b36c3670",
+    "url": "/static/js/main.424efd54.chunk.js"
+  },
+  {
+    "revision": "099452626a31956fb239",
+    "url": "/static/js/runtime-main.0b1aacfe.js"
+  }
+]);
