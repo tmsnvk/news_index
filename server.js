dfefcc5d76a565b7b4bfc2cdccbaa2137139c43b
tmsnvk <contact@tamasnovak.net>
2020-07-30 21:19:24 +0200

    heroku push

--- server.js
+++ server.js
@@ -26,6 +26,14 @@ app.get("/country/:countryId/category/:categoryId", async (req, res) => {
   }
 });
 
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
