d1c99fbb6798df875888fbc5d37175ec39fdcdc7
tmsnvk <contact@tamasnovak.net>
2020-08-22 18:18:16 +0200

    heroku push

--- routes/fetchData.js
+++ routes/fetchData.js
@@ -4,14 +4,12 @@ const axios = require("axios");
 
 router.get("/country/:countryId/category/:categoryId", async (request, response) => {
   try {
-    const { data } = await axios({
-      method: "get",
-      url: "https://newsapi.org/v2/top-headlines",
+    const { data } = await axios.get("https://newsapi.org/v2/top-headlines", {
       params: {
         country: request.params.countryId,
         category: request.params.categoryId,
         apiKey: process.env.REACT_APP_API_KEY
-      },
+      }
     });
     
     response.send(data.articles);
