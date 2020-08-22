8a3e2c735f4b3ab9bc381a816f99bb27e7c80d75
tmsnvk <contact@tamasnovak.net>
2020-08-22 18:13:03 +0200

    small updates

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(publicPath));
 
 app.use("/", require("./routes/fetchData"));
